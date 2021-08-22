3e84906487af2bba472b58c5ae904741f50f5d58
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-08-22 16:16:23 -0500

    saving work, worked on mutations

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,6 +4,22 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+
+const client = ApolloClient ({
+  request: operation => {
+    const toekn = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  url: '.graphlq'
+});
+
 function App() {
   return (
     <Router>
